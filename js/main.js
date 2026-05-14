21deefb605ec93822261e50fc5c6952bd6213f20
Nxstairs <n.shota1624@gmail.com>
2026-05-14 16:43:03 +0900

    update page

--- js/main.js
+++ js/main.js
@@ -11,20 +11,33 @@ const track = document.querySelector(".carypto-track");
 const slides = document.querySelectorAll(".carypto-card");
 
 let index = 0;
-const slideWidth = 220 + 28; // ƒJ[ƒh•220px + gap28px
 
 setInterval(() => {
+
+  const slideWidth =
+    slides[0].offsetWidth + 28;
+
   index++;
 
-  track.style.transition = "transform 0.7s ease";
-  track.style.transform = `translateX(-${index * slideWidth}px)`;
+  track.style.transition =
+    "transform 0.7s ease";
 
-  if (index >= slides.length - 5) {
+  track.style.transform =
+    `translateX(-${index * slideWidth}px)`;
+
+  if(index >= slides.length - 1){
+
     setTimeout(() => {
+
       track.style.transition = "none";
+
       index = 0;
-      track.style.transform = "translateX(0)";
+
+      track.style.transform =
+        "translateX(0)";
+
     }, 700);
+
   }
 
 }, 3000);
